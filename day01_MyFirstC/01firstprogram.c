521e4f24430136f635447ff63d39f3f0f73ef6dc
龙海 <L@longhaideMacBook-Pro.local>
2015-09-14 23:51:58 +0800

    my first c

---
+++ day01_MyFirstC/01firstprogram.c
@@ -0,0 +1,22 @@
+//
+//  01firstprogram.c
+//  
+//
+//  Created by 龙海 on 15/9/14.
+//
+//
+
+#include "01firstprogram.h"
+#include <stdio.h>
+
+int play()
+{
+    return 0;
+}
+
+int main()
+{
+    play();
+    printf("第一个c语言程序\n");
+    return 0;
+}
